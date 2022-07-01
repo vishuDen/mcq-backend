c2fbd7c0bae7997b94f91fc44754803a24d4a4fe
vishalDenzil26 <vishal@touchkin.com>
2022-07-02 01:00:27 +0530

    Changes

--- Backend/server.js
+++ Backend/server.js
@@ -21,9 +21,8 @@ app.use("/api/user",userRoutes)
 // --- deployment
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
     app.get('*', (req,res) => {
-        console.log("yes")
         res.sendFile(path.resolve(__dirname, "frontend", "build" , "index.html"))
     })
 }
